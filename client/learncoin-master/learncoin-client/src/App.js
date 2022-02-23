1fd82a9e17631176c9893702637f230647adf61a
Y.J. Ha <youngha.me@gmail.com>
2022-02-23 17:03:42 -0600

    feat: added the homepage

---
+++ client/learncoin-master/learncoin-client/src/App.js
@@ -0,0 +1,22 @@
+import Module from "./components/Module";
+import Send from "./components/Send";
+import Redirect1 from "./components/Redirect1";
+import Redirect2 from "./components/Redirect2"
+
+function App() {
+  return (
+    <div>
+      <h1> LearnCoin </h1>
+      <Module text="WALLET" />
+      <Send />
+      <img
+        className="banner"
+        src="https://drive.google.com/uc?export=download&id=113hYCr2JAlQ4Ym5RMujNNEQa2rLCqmh3"
+      />
+      <Redirect1 />
+      <Redirect2 />
+      </div>
+  );
+}
+
+export default App;
---
+++ client/learncoin-master/learncoin-client/src/components/Function.js
@@ -0,0 +1,8 @@
+function Function(props) {
+    return (
+        <div>
+            <input type="text" />
+        </div>
+    );
+}
+export default Function;
