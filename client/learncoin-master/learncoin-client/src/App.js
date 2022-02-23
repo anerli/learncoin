5c52b63e90f321a0b5062b7f0b05adc9cb5bc3b7
Moriah Zimmerman <moriahz@iastate.edu>
2022-02-23 17:30:14 -0600

    feat: added a sign in page

--- client/learncoin-master/learncoin-client/src/App.js
+++ client/learncoin-master/learncoin-client/src/App.js
@@ -1,22 +1,9 @@
-import Module from "./components/Module";
-import Send from "./components/Send";
-import Redirect1 from "./components/Redirect1";
-import Redirect2 from "./components/Redirect2"
-
 function App() {
-  return (
-    <div>
-      <h1> LearnCoin </h1>
-      <Module text="WALLET" />
-      <Send />
-      <img
-        className="banner"
-        src="https://drive.google.com/uc?export=download&id=113hYCr2JAlQ4Ym5RMujNNEQa2rLCqmh3"
-      />
-      <Redirect1 />
-      <Redirect2 />
-      </div>
-  );
+    return (
+        <div>
+            <h2>This is the sign in page</h2>
+        </div>
+    );
 }
 
 export default App;
