896c08347bec57f8163bf52233310f692b707579
Anders Lie <anders.erik.lie@gmail.com>
2022-02-24 17:33:06 -0600

    comments

--- server/server.py
+++ server/server.py
@@ -40,6 +40,9 @@ async def receive_chain(request):
     other_len = len(other_chain)
     my_len = len(chain)
     if other_len <= my_len:
+        # FIXME Future Problem: if chains are same length but carry different transactions and proofs then each chain will
+        # be valid but have different blocks at certain points.
+        # One way to fix would be finding a way to merge the chains and have both nodes agree on that one.
         msg = f'Received chain of length {other_len} is not longer than local chain of length {my_len}.'
         info(msg)
         return text(msg, status=400)
