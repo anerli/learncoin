15a88e00dca8be6eec09ab82093b5e971ec88211
Moriah Zimmerman <moriahz@iastate.edu>
2022-02-24 18:17:41 -0600

    feat: keyPair (broken)

--- client/learncoin-client/src/Pages/Signup.js
+++ client/learncoin-client/src/Pages/Signup.js
@@ -1,7 +1,39 @@
+const crypto = require('crypto');
+
+let inputStyle = {
+    visibility: 'hidden'
+}
+
+function createFunction() {
+    console.log('clicked');
+    document.getElementById('text').style.visibility = 'visible';
+    crypto.generateKeyPair(
+        'ed25519',
+        {
+            publicKeyEncoding: {
+                type: 'spki',
+                format: 'der'
+            },
+            privateKeyEncoding: {
+                type: 'pkcs8',
+                format: 'der'
+            }
+        },
+        (err, publicKey, privateKey) => {
+            console.log("Public key:", publicKey.toString('hex'));
+            console.log("Private key:", privateKey.toString('hex'));
+        }
+    );
+};
+
 const Signup = () => {
     return (
         <div>
             <h1>Sign up for Learncoin here!</h1>
+            <button id='button' onClick={createFunction}>Sign up</button>
+            <p>There is no way to restore a forgotten key!</p>
+            <p id='text' style={inputStyle}>This is your private key: </p>
+
         </div>
     )
 };
