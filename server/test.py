b1a3e192e79521592cfcfd71de90f112083af020
Anders Lie <anders.erik.lie@gmail.com>
2022-03-31 03:01:36 -0500

    issue is that sha256 hashes producing different outputs

--- server/test.py
+++ server/test.py
@@ -21,9 +21,14 @@
 
 from primitives import PrivateKey, serialize_private_key, deserialize_private_key, serialize_public_key
 
-pkey = deserialize_private_key(bytes.fromhex('679df846fdf7118544654392e0b7a4b7473622581b28e1189793de5d694bce64'))
+pkey = deserialize_private_key(bytes.fromhex('63abc05b43c21f8fd158ee24c1c7bd90fbce860358c44c0eb68859b098622181'))
 pubkey = pkey.public_key()
 print(serialize_public_key(pubkey).hex())
+
+pubkey.verify(
+    bytes.fromhex('0c030e9e40e7ac4dbc93a66469f5dfe073bb54d215a6944561aa77135a8f2ab2742d43ddb1d829c220e887ed6ee5cb57d20fd9210de136f6afe51322acf7a601'),
+    bytes.fromhex('a762ae3cc8e23dfd451067573c4e6a43f6668c27924bba48ba305799c7a901ae')
+)
 
 # key = PrivateKey.generate()
 # print(serialize_private_key(key).hex())
