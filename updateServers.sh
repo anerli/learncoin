38824ce6e7ac61da52f9c4f36d12f0520e9104d6
jagust <jagust@iastate.edu>
2022-03-31 18:11:39 +0000

    Update updateServers.sh

--- updateServers.sh
+++ updateServers.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-SCRIPT="cd ..; sudo git clone https://oauth2:glpat-tnmmb9yERsxK5KBrsVfp@git.ece.iastate.edu/lie/learncoin.git; ls; exit"
+SCRIPT="ssh-keygen -R 10.90.73.217; cd ..; sudo git clone https://oauth2:glpat-tnmmb9yERsxK5KBrsVfp@git.ece.iastate.edu/lie/learncoin.git; ls; exit"
 HOSTS=("coms-402-sd-23.class.las.iastate.edu" "coms-402-sd-24.class.las.iastate.edu" "coms-402-sd-25.class.las.iastate.edu" "coms-402-sd-26.class.las.iastate.edu" "coms-402-sd-27.class.las.iastate.edu")
 USERNAME="gitlab"
 for i in ${!HOSTS[*]} ; do
      echo ${HOSTS[i]}
-     ssh-keygen -R 10.90.73.217
      ssh -ttl ${USERNAME} ${HOSTS[i]} "${SCR}"
 done
